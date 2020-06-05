91c4a65b025b0aa994b82b4721f58481918db292
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-06 02:01:24 +0530

    Provider added

--- src/routes/QueRoutes/index.js
+++ src/routes/QueRoutes/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import ModalProvider from "../../contexts/ModalContext";
 import AddQueForm from "../../components/createQuestions/AddQueForm";
 
 const QueRoutes = () => {
   return (
     <>
-      <Route exact path="/add-questions" component={AddQueForm} />
+      <ModalProvider>
+        <Route exact path="/add-questions" component={AddQueForm} />
+      </ModalProvider>
     </>
   );
 };

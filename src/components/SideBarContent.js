f340c8a8d340ee326c825102a67ee5b6ce673ed1
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-07 03:25:54 +0530

    SIDEBAR: gave sidebar buttons links to perticular route

--- src/components/SideBarContent.js
+++ src/components/SideBarContent.js
@@ -1,17 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Menu } from "antd";
-
-import { FileTextOutlined } from "@ant-design/icons";
+import { QuestionOutlined, DashboardOutlined } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 
 const SideBarContent = () => {
   return (
     <>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        <SubMenu key="sub1" icon={<FileTextOutlined />} title="Questions">
-          <Menu.Item key="3">Create Questions</Menu.Item>
-          <Menu.Item key="4">View QUestions</Menu.Item>
+      <Menu theme="dark" mode="inline">
+        <SubMenu key="sub1" title="Dashboard" icon={<DashboardOutlined />}>
+          <Menu.Item key="1">
+            <Link to="/dashboard">
+              <span>Dashboard</span>
+            </Link>
+          </Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub2" title="Questions" icon={<QuestionOutlined />}>
+          <Menu.Item key="3">
+            <Link to="/dashboard/create-question">Create Questions</Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Link to="/dashboard/view-question">View QUestions</Link>
+          </Menu.Item>
         </SubMenu>
       </Menu>
     </>

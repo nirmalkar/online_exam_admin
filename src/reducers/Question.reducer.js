85cd419946f448b511ce61f47f2b992fa227a90e
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-06 20:51:08 +0530

    ADDED_IMPORTANT_ATTRIBUTE: in border radius

---
+++ src/reducers/Question.reducer.js
@@ -0,0 +1,13 @@
+// import { v4 as uuid } from "uuid";
+// const INIT_STATE = {
+//   question: {},
+// };
+// const questionReducer = (state, action) => {
+//   switch (action.type) {
+//     case "ADD_QUESTION":
+//       return [
+//         ...state,
+//         { id: uuid, question: action.question, marks: action.marks },
+//       ];
+//   }
+// };
